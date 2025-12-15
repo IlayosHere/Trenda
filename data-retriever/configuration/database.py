7a06097099a83bb0bc327e0669231d1c9de5732d
Goshe-ai <arielgoshe@gmail.com>
2025-12-15 10:57:11 +0200

    feat: Add initial PostgreSQL database configuration using environment variables.

--- data-retriever/configuration/database.py
+++ data-retriever/configuration/database.py
@@ -1,8 +1,10 @@
+import os
+
 POSTGRES_DB = {
-    "dbname": "trenda",
-    "user": "postgres",
-    "password": "heblish123",
-    "host": "localhost",
-    "port": "5432",
-    "options": "-c search_path=trenda"
+    "dbname": os.getenv("DB_NAME", "trenda"),
+    "user": os.getenv("DB_USER", "postgres"),
+    "password": os.getenv("DB_PASSWORD"),
+    "host": os.getenv("DB_HOST", "localhost"),
+    "port": os.getenv("DB_PORT", "5432"),
+    "options": os.getenv("DB_OPTIONS", "-c search_path=trenda"),
 }
