a0ee3a18008d468df057e39ea82adff50f2b71eb
Goshe-ai <arielgoshe@gmail.com>
2026-01-03 22:46:52 +0200

    Change MT5_EXPIRATION_MINUTES to 5 minutes

--- data-retriever/configuration/broker.py
+++ data-retriever/configuration/broker.py
@@ -18,7 +18,7 @@ TWELVEDATA_BASE_URL: str = os.getenv("TWELVEDATA_BASE_URL", "https://api.twelved
 MT5_MAGIC_NUMBER: int = int(os.getenv("MT5_MAGIC_NUMBER", "123456"))
 MT5_DEVIATION: int = int(os.getenv("MT5_DEVIATION", "20"))
 MT5_DEFAULT_LOT_SIZE: float = float(os.getenv("MT5_DEFAULT_LOT_SIZE", "0.01"))
-MT5_EXPIRATION_MINUTES: int = int(os.getenv("MT5_EXPIRATION_MINUTES", "10"))
+MT5_EXPIRATION_MINUTES: int = int(os.getenv("MT5_EXPIRATION_MINUTES", "5"))
 
 # Trading Strategy Config
 TRADE_QUALITY_THRESHOLD: float = float(os.getenv("TRADE_QUALITY_THRESHOLD", "0.5"))
